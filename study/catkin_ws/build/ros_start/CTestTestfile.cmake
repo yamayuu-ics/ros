512fb6a61bd626711afdd9d50ecd992c8ca527e2
yamayuu <yamayuu.ics@gmail.com>
2018-05-27 15:20:20 +0900

    ros chat

---
+++ study/catkin_ws/build/ros_start/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yamayuu/work/ros/study/catkin_ws/src/ros_start
+# Build directory: /home/yamayuu/work/ros/study/catkin_ws/build/ros_start
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
