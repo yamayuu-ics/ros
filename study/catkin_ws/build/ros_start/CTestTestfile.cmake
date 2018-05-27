a975ee27d0039a42d9e7d3cd4374e8c93aee7003
yamayuu <yamayuu.ics@gmail.com>
2018-05-27 15:27:58 +0900

    reflect  .gitignore

--- study/catkin_ws/build/ros_start/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yamayuu/work/ros/study/catkin_ws/src/ros_start
-# Build directory: /home/yamayuu/work/ros/study/catkin_ws/build/ros_start
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
